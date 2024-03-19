493a35b873ec87e57287041b7a7abbc9d98dac27
Kyomik <almrilhamm@gmail.com>
2024-03-19 11:08:10 +0800

    config

--- config.php
+++ config.php
@@ -22,8 +22,7 @@ error_reporting(0);
 	$host 	= 'localhost'; // host server
 	$user 	= 'root';  // username server
 	$pass 	= 'ammarjie500'; // password server, kalau pakai xampp kosongin saja
-	$dbname = 'db_toko'; // nama database anda
-	//    
+	$dbname = 'db_tokoo'; // nama database anda
 	
 	try{
 		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
