3ca9fa1166bc8ec0b99e1d7ef62458749902c14e
ARDGenk <ardbee2710@gmail.com>
2024-03-25 20:15:01 +0800

    babi

--- admin/template/sidebar.php
+++ admin/template/sidebar.php
@@ -5,7 +5,7 @@
     // $hasil_profil = $lihat -> member_edit($id);
 ?>
 <!-- Sidebar -->
-<ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: #FFBC40; background-image: linear-gradient(180deg, #FFA500 10%, #FFD280 100%); background-size: cover;">
+<ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: #FFBC40; background-image: linear-gradient(180deg, #FF8911 10%, #FFBC40 100%); background-size: cover;">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
